d5a679e61dc315ed00aeba26045392029d3f96b1
Thomas Cieslar <thomas@cieslar.io>
2021-08-05 16:52:00 +0200

    Add Test component that shows the errors

---
+++ components/Test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    foo: {{ foo }}
+    bar: {{ bar }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  name: 'TsComponent',
+  props: {
+    foo: {
+      type: String as PropType<string>,
+      default: 'foo'
+    }
+  },
+  computed: {
+    bar () {
+      return this.foo + 'bar'
+    }
+  },
+  mounted () {
+    console.log(this.foo)
+    console.log(this.bar)
+    console.log(this.$axios)
+    console.log(this.$el)
+  }
+})
+</script>
